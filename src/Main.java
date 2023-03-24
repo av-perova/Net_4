f17ccb0e9671542b9a7ad20d708e96a36a0169df
perova test <av_perova@mail.com>
2023-03-24 23:48:21 +0700

    deposit bonus

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int balance = 100;
+        int deposit = 1100;
+        int minDeposit = 1000;
+        int rate = 100;
+        int bonus = 0;
+
+        if (deposit > minDeposit) {
+            bonus = deposit / rate;
+            balance = balance + deposit + bonus;
+            System.out.println("Ваш баланс " + balance + ". Вам начислено " + bonus + " бонусных рублей.");
+        } else {
+            balance = balance + deposit;
+            System.out.println("Ваш баланс " + balance + ". Вам начислено " + bonus + " бонусных рублей.");
+        }
     }
 }
